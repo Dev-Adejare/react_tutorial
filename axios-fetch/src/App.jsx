bdd35f2661892a4089d3c5083316ab53408956f7
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-04-15 10:56:29 +0100

    update

--- axios-fetch/src/App.jsx
+++ axios-fetch/src/App.jsx
@@ -45,16 +45,23 @@ function App() {
   }, [posts, search])
   
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
     const id = posts.length ? posts[posts.length -1].id + 1 : 1
     const dateTime = format (new Date(), 'MMMM dd, yyyy pp' )
-    const newPost = {id, title: postTitle, dateTime, body: PostBody }
-    const allPosts = [...posts, newPost]
-    setPosts(allPosts)
-    setPostTitle('')
-    setPostBody('')
-    navigate('/')
+    const newPost = {id, title: postTitle, dateTime, body: PostBody };
+
+    try {
+      const response = await api.post("/posts", newPost)
+      const allPosts = [...posts, newPost]
+      setPosts(allPosts)
+      setPostTitle('')
+      setPostBody('')
+      navigate('/')
+    } catch (error) {
+      
+    }
   }
   const handleDelete = (id) => {
     const postList = posts.filter((post) => post.id !== id);
