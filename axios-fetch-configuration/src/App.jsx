ac8d7f43cd2469f711a5ca1504c0bb91c6b34613
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-04-16 11:07:45 +0100

    update

--- axios-fetch-configuration/src/App.jsx
+++ axios-fetch-configuration/src/App.jsx
@@ -12,6 +12,7 @@ import { format } from "date-fns";
 import api from "./api/post";
 import EditPost from "./EditPost";
 import useWindowSize from "../hooks/useWindowSize";
+import useAxiosFetch from "../hooks/useAxiosFetch";
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -23,32 +24,33 @@ function App() {
   const [postBody, setPostBody] = useState("");
   const [editPostTitle, setEditPostTitle] = useState("");
   const [editPostBody, setEditPostBody] = useState("");
-  const {width} = useWindowSize()
+  const {width} = useWindowSize();
+  const { data, fetchError, isLoading } = useAxiosFetch("http://localhost::4000/posts")
 
   //STEP 1
-  useEffect(() => {
-    //we are using useEffect hooks so that the function will take effect as soon as the page loads
-    const fetchPosts = async () => {
-      try {
-        // const response = await axios.get("/endpoint")
-        const response = await api.get("/posts"); //axios returns results in json format automatically, so we don't need to convert the response to json format
-        // {response = {
-        // data : {}
-        // }}
-        setPosts(response.data); //setPosts array updated
-      } catch (err) {
-        if (err.response) {
-          // Not in the 200 response range
-          console.log(err.response.data);
-          console.log(err.response.status);
-          console.log(err.response.headers);
-        } else {
-          console.log(`Error: ${err.message}`);
-        }
-      }
-    };
-    fetchPosts();
-  }, []);
+  // useEffect(() => {
+  //   //we are using useEffect hooks so that the function will take effect as soon as the page loads
+  //   const fetchPosts = async () => {
+  //     try {
+  //       // const response = await axios.get("/endpoint")
+  //       const response = await api.get("/posts"); //axios returns results in json format automatically, so we don't need to convert the response to json format
+  //       // {response = {
+  //       // data : {}
+  //       // }}
+  //       setPosts(response.data); //setPosts array updated
+  //     } catch (err) {
+  //       if (err.response) {
+  //         // Not in the 200 response range
+  //         console.log(err.response.data);
+  //         console.log(err.response.status);
+  //         console.log(err.response.headers);
+  //       } else {
+  //         console.log(`Error: ${err.message}`);
+  //       }
+  //     }
+  //   };
+  //   fetchPosts();
+  // }, []);
 
   useEffect(() => {
     const filteredResults = posts.filter(
