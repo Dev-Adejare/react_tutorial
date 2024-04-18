617192a72d77da47744cf6224b90abe14eb02861
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-04-18 10:54:22 +0100

    update

--- axios-fetch-configuration/src/context/DataContext.jsx
+++ axios-fetch-configuration/src/context/DataContext.jsx
@@ -1,9 +1,7 @@
 import { createContext, useState, useEffect, Children } from "react";
-import { useNavigate } from "react-router-dom";
-import { format } from "date-fns";
-import api from "../api/post";
-import useWindowSize from "../hooks/useWindowSize";
 import useAxiosFetch from "../hooks/useAxiosFetch";
+
+
 
 
 const DataContext = createContext({})
@@ -12,12 +10,11 @@ export const DataProvider = ({children}) => {
 
     const [posts, setPosts] = useState([]);
 
-  const navigate = useNavigate();
+  
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   
-  const [editPostTitle, setEditPostTitle] = useState("");
-  const [editPostBody, setEditPostBody] = useState("");
+
   
   const { data, fetchError, isLoading } = useAxiosFetch("http://localhost:4000/posts");
 
@@ -38,43 +35,14 @@ export const DataProvider = ({children}) => {
   }, [posts, search]);
 
  
-  const handleDelete = async (id) => {
-    try {
-      await api.delete(`/posts/${id}`);
-      const postList = posts.filter((post) => post.id !== id);
-      setPosts(postList);
-      navigate("/");
-    } catch (error) {
-      console.log(`Error: ${error.message}`);
-    }
-  };
+  
 
-  const handleEdit = async (id) => {
-    const dateTime = format(new Date(), "MMMM dd, yyyy pp");
-    const updatedPost = {
-      id,
-      title: editPostTitle,
-      dateTime,
-      body: editPostBody,
-    };
-    try {
-      const response = await api.put(`/posts/${id}`, updatedPost);
-      setPosts(
-        posts.map((post) => (post.id === id ? { ...response.data } : post))
-      );
-      setEditPostTitle("");
-      setEditPostBody("");
-      navigate("/");
-    } catch (error) {
-      console.log(`Error: ${error.message}`);
-    }
-  };
+ 
 
     return (
         <DataContext.Provider value= {{
             search, setSearch, searchResults, fetchError, isLoading,
-            posts, setPosts,handleEdit,editPostBody,setEditPostBody, editPostTitle, 
-            setEditPostTitle, handleDelete
+            posts, setPosts
             
 
 
